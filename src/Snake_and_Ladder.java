468385e62f66321d46fc6bd42eb7d2de0c1a7e09
Nisha7781 <nishamali0781@gmail.com>
2024-03-11 23:12:44 +0530

    repeat till 100, restart the game of score goes below 0

--- src/Snake_and_Ladder.java
+++ src/Snake_and_Ladder.java
@@ -5,30 +5,40 @@ public class Snake_and_Ladder {
         System.out.println("Welcome to Snake and ladder game with single player...");
         int PlayersPosition = 0;
 
-        int dice = (int) (Math.random()*6+1);
-        System.out.println("Dice rolls by: "+ dice);
-
-        int currentOption = (int)(Math.random()*3+1);
-        switch(currentOption)
+        while(PlayersPosition != 100)
         {
-            case 1:
-                System.out.println("NO PLAY: "+ PlayersPosition);
-                System.out.println("Player stays in the same position");
-                System.out.println("Position of player 1 is : "+ PlayersPosition);
-                break;
+            int dice = (int) (Math.random()*6+1);
+            System.out.println("Dice rolls by: "+ dice);
 
-            case 2:
-                System.out.println("Ladder: "+ (PlayersPosition + dice));
-                System.out.println("Player moves forward by "+dice);
-                PlayersPosition = PlayersPosition + dice;
-                System.out.println("Position of player 1 is: "+ PlayersPosition);
-                break;
+            int currentOption = (int)(Math.random()*3+1);
+            switch(currentOption)
+            {
+                case 1:
+                    System.out.println("NO PLAY");
+                    System.out.println("Player stays in the same position");
+                    System.out.println("Position of player 1 is : "+ PlayersPosition);
+                    break;
 
-            case 3:
-                System.out.println("Snake: "+ (PlayersPosition-dice));
-                System.out.println("Player moves backword by "+dice);
-                PlayersPosition = PlayersPosition - dice;
-                System.out.println("Position of player 1 is: "+ PlayersPosition);
+                case 2:
+                    System.out.println("Ladder");
+                    System.out.println("Player moves forward by "+dice);
+                    PlayersPosition = PlayersPosition + dice;
+                    System.out.println("Position of player 1 is: "+ PlayersPosition);
+                    break;
+
+                case 3:
+                    System.out.println("Snake");
+                    System.out.println("Player moves backword by "+dice);
+                    PlayersPosition = PlayersPosition - dice;
+                    System.out.println("Position of player 1 is: "+ PlayersPosition);
+            }
+            System.out.println(" ");
+
+            if(PlayersPosition < 0)
+            {
+                PlayersPosition = 0;
+            }
+
         }
     }
 }

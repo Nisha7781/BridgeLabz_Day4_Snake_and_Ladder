b2ef556c9ea5628c74d9a1eb51a2babf70ddc4b6
Nisha7781 <nishamali0781@gmail.com>
2024-03-11 22:56:45 +0530

    Start the player 1 at position 0

--- src/Snake_and_Ladder.java
+++ src/Snake_and_Ladder.java
@@ -2,6 +2,7 @@
 public class Snake_and_Ladder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and ladder game with single player...");
-
+        int PlayersPosition = 0;
+        System.out.println("Position of player 1: "+ PlayersPosition);
     }
 }
